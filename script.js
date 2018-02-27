7e440ade06b68b0499f5af6227323e87b2f3af56
kalen pecson <kalenpecson@students.berkeley.net>
2018-02-27 10:52:37 -0800

    s

--- script.js
+++ script.js
@@ -18,18 +18,27 @@ $(document).ready(function(){
             }
         });
     });
+    $("tr").on("mouseover",function(){
+        $(this).animate({"height":"30px"},)
+    });
 });
 
 function myFunction(result){
     var song="";
     var album="";
     var picture="";
+    var audio="";
     for (var i=0; i<count; i++){
         album= result.results[i].collectionName;
         song= result.results[i].trackName;
-        picture= result.results[i].artworkUrl30;
+        picture= result.results[i].artworkUrl100;
+        audio=result.results[i].previewUrl;
         document.getElementById("table").innerHTML+="<tr>";
-        document.getElementById("table").innerHTML+="<td id='rank'>"+(i+1)+"</td>"+"<td id='img'><img src="+picture+"></td>"+"<td id='info'>"+song+"<br>"+album+"</td>";
+        document.getElementById("table").innerHTML+=
+            "<td id='rank'>"+(i+1)+"</td>"+
+            "<td id='img'><img src="+picture+ "></td>"+
+            "<td id='info'>"+song+"<br>"+album+"</td>"+
+            "<td id='audio'><audio controls src="+audio+" type= 'audio/m4a'></audio></td>";
     }
 }
 
